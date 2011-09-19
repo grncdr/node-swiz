4ab155fec2a8077b50744f388d936bf18f22e4db
Tomaz Muraus <tomaz@tomaz.me>
2011-09-19 09:38:56 +0200

    Update demo.js.

--- demo.js
+++ demo.js
@@ -1,15 +1,24 @@
-var Swiz = require('swiz').Swiz;
-var Valve = require('swiz').Valve;
-var Chain = require('swiz').Chain;
-var defToValve = require('swiz').defToValve;
+var swiz = require('./lib/swiz');
+var Swiz = require('./lib/swiz').Swiz;
+var Valve = require('./lib/swiz').Valve;
+var Chain = require('./lib/swiz').Chain;
+var defToValve = require('./lib/swiz').defToValve;
+var O = swiz.struct.Obj;
+var F = swiz.struct.Field;
 
 // Note: There's one set of definitions that control both serialization
 // and validation
-var def = {
-'Node': [
-  ['key', {'val' : new Chain()}],
-  ['ip_address_v4', {'val' : new Chain().isIP()}]
-]};
+var def = [
+  O('Node',
+    {
+      'fields': [
+        F('key', {'val' : new Chain()}),
+        F('ip_address_v4', {'val' : new Chain().isIP()})
+      ],
+
+      'plural': 'nodes'
+    })
+];
 
 var validity = defToValve(def);
 var schema = validity.Node;
@@ -25,13 +34,13 @@ console.log('validate a payload:\n');
 
 // Validate the generic payload
 v.check(CreatePayload, function(cleaned, err) {
-  var swiz = new Swiz(def);
+  var sw = new Swiz(def);
   if (err) {
     console.error(err);
   } else {
     console.log('\n\nserialize an object\n');
     cleaned.getSerializerType = function() {return 'Node';};
-    sw.serialize(validate.SERIALIZATION.SERIALIZATION_JSON, 1, cleaned,
+    sw.serialize(swiz.SERIALIZATION.SERIALIZATION_JSON, 1, cleaned,
       function(err, results) {
         console.log(results);
       }
